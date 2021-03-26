5b632d27cebd64b127dd362d24e796e16ee1fed5
peto123455 <peto123455@gmail.com>
2021-03-26 09:11:35 +0100

    Fixed users table scaling, added username and password verification, etc.

--- page/auth.js
+++ page/auth.js
@@ -19,7 +19,11 @@ exports.registerPost = async (req, res, next) => {
             req.flash("error", "User already exists !");
         else if(req.body.password != req.body.passwordRepeat) 
             req.flash("error", "Passwords don't match !");
-        else 
+        else if(!checkLength(4, 32, req.body.username) || !checkLength(6, 32, req.body.password)) 
+            req.flash("error", "Username - 4 to 32 characters long.<br>Password - 6 to 32 characters long.");
+        else if(!isValid(req.body.username) || !isValid(req.body.password)) 
+            req.flash("error", "Incorrect characters !");
+        else
         {
             const hash = await bcrypt.hash(req.body.password, 5);
             const user = new User({ "username": req.body.username, "password": hash });
@@ -38,4 +42,12 @@ exports.logout = (req, res) => {
         if (err) console.log(err);
         res.redirect('/');
     });
+}
+
+function isValid(text) {
+    return /^[0-9a-zA-Z_.-]+$/.test(text);
+}
+
+function checkLength(minlength, maxlength, text) {
+    return (text.length >= minlength && text.length <= maxlength);
 }
